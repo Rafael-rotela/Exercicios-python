11007ea5b58a2ee7752a7983e0a8ef415307b5e0
Rafael Rotela <rjvrafael22@gmail.com>
2026-01-27 08:03:46 -0400

    pequena alteração

--- lista05/53exercicio.py
+++ lista05/53exercicio.py
@@ -1,22 +1,28 @@
-print('-'*10,'BemMaisFort','-'*10)
+print('*'*10, 'BemMaisFort', '-'*10)
+
 i = 0
-
-
 grupo = []
 
 while True:
     pessoa = {
-        'idade' : int(input('digite sua idade: ')),
-        'Sexo' : input('digite seu sexo (M/F):').lower(),
-        'Altura' : float(input('digite sua altura: ')),
-        'Peso' : float(input('digite sua peso:'))
+        'idade': int(input('digite sua idade: ')),
+        'sexo': input('digite seu sexo (M/F): ').lower(),
+        'altura': float(input('digite sua altura: ')),
+        'peso': float(input('digite seu peso: '))
     }
-    print('novo cadastro')
+
     grupo.append(pessoa)
     i += 1
+
     if i == 2:
         break
 
-
 pesos = [p['peso'] for p in grupo]
-print('O maior peso é: ', max(pesos))
+print('O maior peso é:', max(pesos))
+
+idades = [p['idade'] for p in grupo]
+print('A maior idade é:', max(idades))
+
+alturas = [p['altura'] for p in grupo]
+print('A maior altura é:', max(alturas))
+print('A menor altura é:', min(alturas))
